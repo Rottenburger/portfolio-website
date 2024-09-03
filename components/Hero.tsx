037e90b1bf82549952625071ea33cdf0f12dc897
tpr3 <tpr3@aber.ac.uk>
2024-09-03 12:25:53 +0100

    Created grid and dark mode styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,17 +12,21 @@ const Hero = () => {
             w-[50vw]" fill-blue' />
         </div>
 
-        <div className="h-[50rem] w-full dark:bg-black bg-white  
-        dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  
+        dark:bg-grid-white/[0.2] bg-grid-black/[0.3] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center
-       dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+       dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 
-       </div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text 
-        text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Backgrounds
-        </p>
+       <div className='flex justify-center'>
+        <div className='max-w-[89vw] md:max-w-2xl
+        lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest
+          text-xs text-center text-blue-100 max-w-80'>
+            Web page with NextJS
+          </h2>
+        </div>
+        </div>
       </div>
     </div>
   )
