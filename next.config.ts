bece1c3f546a33e103448ada9d5f2982226ea56e
tpr3 <roethenbaughthomas@gmail.com>
2025-08-27 21:20:00 +0100

    fix image error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yvakkffn8okazqqg.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
